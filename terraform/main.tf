5b31da595db7cf4e881259d2d29c37d70277412b
Bishoy Samwel <bishoy.samwuel@gmail.com>
2025-05-14 12:20:11 +0300

    add: 1 vpc with 3 public subnets, 3 private subnets, on 3 AZs

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_region" "current" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_env"
+    Terraform   = "true"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  for_each = var.private_subnets
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.cidr_index)
+  availability_zone = data.aws_availability_zones.available.names[each.value.az_index]
+
+  tags = {
+    Name        = each.key
+    Environment = "demo"
+    Terraform   = "true"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  for_each = var.public_subnets
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value.cidr_index)
+  availability_zone = data.aws_availability_zones.available.names[each.value.az_index]
+
+  tags = {
+    Name        = each.key
+    Environment = "demo"
+    Terraform   = "true"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "vpc"
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet-1" = { cidr_index = 1, az_index = 0 }
+    "private_subnet-2" = { cidr_index = 2, az_index = 1 }
+    "private_subnet-3" = { cidr_index = 3, az_index = 2 }
+  }
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet-1" = { cidr_index = 10, az_index = 0 }
+    "public_subnet-2" = { cidr_index = 11, az_index = 1 }
+    "public_subnet-3" = { cidr_index = 12, az_index = 2 }
+  }
+}
+
